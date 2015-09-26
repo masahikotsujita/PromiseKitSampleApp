756ef91369a06943195ef238fb1506848941caed
Masahiko Tsujita <masahikot.uec@icloud.com>
2015-09-26 22:52:14 +0900

    Implemented

---
+++ Podfile
@@ -0,0 +1,9 @@
+source "https://github.com/CocoaPods/Specs.git"
+platform :ios, "9.0"
+use_frameworks!
+
+target "PromiseKitSampleApp" do
+    pod "Alamofire"
+    pod "PromiseKit", :git => "https://github.com/mxcl/PromiseKit.git", :tag => "swift-2.0-beta5"
+end
+
--- PromiseKitSampleApp/ViewController.swift
+++ PromiseKitSampleApp/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Alamofire
+import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var webView: UIWebView!
+    
+    let errorDomain = "jp.tsujitamasahiko.PromiseKitSampleApp.ViewController.ErrorDomain"
+    enum ErrorCode: Int {
+        case InvalidResponseData
     }
-
+    
+    @IBAction func loadWebPage(sender: UIButton) {
+        guard let url = self.urlField.text else {
+            return
+        }
+        if self.urlField.isFirstResponder() {
+            self.urlField.resignFirstResponder()
+        }
+        Promise<String> { (fulfill, reject) -> Void in
+            request(.GET, url).responseString(completionHandler: { (request, response, result) -> Void in
+                if let html = result.value {
+                    fulfill(html)
+                } else {
+                    reject(NSError(domain: self.errorDomain, code: ErrorCode.InvalidResponseData.rawValue, userInfo: nil))
+                }
+            })
+        }.then { (html) in
+            self.webView.loadHTMLString(html, baseURL: NSURL(string: url))
+        }.report { (error) in
+            let alert = UIAlertController(title: "Error", message: "Failed to loading page: \(self.urlField.text)", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        }
+    }
 
 }
-
