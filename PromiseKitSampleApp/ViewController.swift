97e5824191ed9c040364941948afe6c4cb6aa4d2
Masahiko Tsujita <masahikot.uec@icloud.com>
2015-09-26 22:38:43 +0900

    Initial Commit

---
+++ PromiseKitSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PromiseKitSampleApp
+//
+//  Created by 辻田晶彦 on 2015/09/26.
+//  Copyright © 2015年 Masahiko Tsujita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
